ecc53f3fb43a51cd9c2643ea551d74b23d3c7025
finkegabriel <finkegabriel@gmail.com>
2018-11-22 20:01:59 -0700

    init commit

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const dbConfig = require('./database.js');
 //this is an example file of where and when you would implement the features from this project
 
 const dbCon = (
-    dbConfig.Host='localhost',
-    dbConfig.Database='ff',
-    dbConfig.User='root',
+    dbConfig.Host='',
+    dbConfig.Database='',
+    dbConfig.User='',
     dbConfig.Password=''
 );
-
 
